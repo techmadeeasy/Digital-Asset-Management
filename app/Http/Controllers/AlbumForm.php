7cb110932b27f8d91bddba4a8c9913749e325a4d
techmadeeasy <42980733+techmadeeasy@users.noreply.github.com>
2019-01-29 12:52:55 +0200

    commented line
    I commented the line that submits the category name, bacause that column isn't created in the database yet

--- app/Http/Controllers/AlbumForm.php
+++ app/Http/Controllers/AlbumForm.php
@@ -28,7 +28,7 @@ if($request->hasFile('image')){
 $filepath = date('Y-m-d') . "/" . $fileName;
       $albumtable = new Form;
       $albumtable->name = $request->get('albumname');
-      $albumtable->category = $request->get('cat');
+     // $albumtable->category = $request->get('cat');
       $albumtable->edition = $request->get('albumedition');
       $albumtable->photographer = $request->get('albumphoto');
         $albumtable->description = "nothihn";
