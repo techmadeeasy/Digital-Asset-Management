9acae2d0643051acbe9eadcd2d340d37ad8d866f
jerryboy <djerryboy2@gmail.com>
2019-01-30 13:42:01 +0200

    added a album column via migratioon file for production server

---
+++ database/migrations/2019_01_30_113908_add_to_image_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddToImageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('image', function (Blueprint $table) {
+            $table->string("album")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('image', function (Blueprint $table) {
+            //
+        });
+    }
+}
